17a59d9abf5dc931061f1becee4efd66e3bfbc39
Alviannn <alvianndq@gmail.com>
2022-12-14 23:21:34 -0800

    feat(users): add user entity

---
+++ Teddit/Entities.swift
@@ -0,0 +1,15 @@
+//
+//  Entities.swift
+//  Teddit
+//
+//  Created by prk on 12/14/22.
+//
+
+import Foundation
+
+struct User {
+    var name: String
+    var email: String
+    var password: String
+}
+
