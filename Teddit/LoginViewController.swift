024ca0c4ae43c671a603c00635d13ed53d1ac8ae
Alviannn <alvianndq@gmail.com>
2022-12-20 22:47:32 -0800

    feat(login): add logic when logging in

--- Teddit/LoginViewController.swift
+++ Teddit/LoginViewController.swift
@@ -9,12 +9,52 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet var usernameField: UITextField!
+    @IBOutlet var passwordField: UITextField!
+    
+    var userRepo: UserRepository!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        userRepo = UserRepository()
     }
     
     @IBAction func unwindToLogin(_ unwindSegue: UIStoryboardSegue) {}
-
-
+    
+    @IBAction func loginPressed(_ sender: Any) {
+        let alertTitle = "Validation Error"
+        
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+        if username.isEmpty {
+            self.showAlert(title: alertTitle, message: "Username cannot be empty.")
+            return
+        }
+        if password.isEmpty {
+            self.showAlert(title: alertTitle, message: "Password cannot be empty.")
+            return
+        }
+        
+        do {
+            let authAlertTitle = "Authentication Error"
+            if let entity = try userRepo.findByUsername(username: username) {
+                if entity.password != password {
+                    self.showAlert(title: authAlertTitle, message: "Password is incorrect.")
+                    return
+                }
+                
+                // TODO: authentication success
+                self.showAlert(title: "Success", message: "Authentication successful.")
+            } else {
+                self.showAlert(title: authAlertTitle, message: "This username is not registered.")
+                return
+            }
+        } catch {
+            print("Cannot fetch for user entity: \(error)")
+        }
+    }
+    
 }
 
