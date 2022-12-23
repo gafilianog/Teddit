9d8230d341b95954ecf36a446cf4039e8a1132d7
juantdefrin94 <jtdefrin33@gmail.com>
2022-12-22 23:21:15 -0800

    Finishing CreateTopicViewController when name changed, desc changed and save data to core data

--- Teddit/CreateTopicViewController.swift
+++ Teddit/CreateTopicViewController.swift
@@ -9,11 +9,62 @@ import UIKit
 
 class CreateTopicViewController: UIViewController, UITextViewDelegate {
     
+    @IBOutlet weak var DescTxt: UITextField!
+    @IBOutlet weak var NameTxt: UITextField!
+    @IBOutlet weak var CreateTopicButton: UIButton!
+    var nameIsEmpty = true
+    var descIsEmpty = true
+    var topicRepo: TopicRepository!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        topicRepo = TopicRepository()
     }
     
-//    func textViewDidChange(_ textView: UITextView) {
+    @IBAction func NameChanged(_ sender: Any) {
+        print("Masuk Name")
+        if(!NameTxt.text!.isEmpty){
+            nameIsEmpty = false
+        }else{
+            nameIsEmpty = true
+        }
+        
+        if(!nameIsEmpty && !descIsEmpty){
+            CreateTopicButton.isEnabled = true
+        }
+    }
+    
+    @IBAction func DescChanged(_ sender: Any) {
+        print("Masuk Desc")
+        if(!DescTxt.text!.isEmpty){
+            descIsEmpty = false
+        }else{
+            descIsEmpty = true
+        }
+        
+        if(!nameIsEmpty && !descIsEmpty){
+            CreateTopicButton.isEnabled = true
+        }
+    }
+    
+    @IBAction func CreateTopicPressed(_ sender: Any) {
+        let nameTxt: String = NameTxt.text!
+        let descTxt: String = DescTxt.text!
+        //nantinya gambar topic bisa di upload juga
+        //save to new topic
+        let entity = topicRepo.create()
+        entity.name = nameTxt
+        entity.desc = descTxt
+        entity.image = "sublogo"
+        
+        do{
+            try topicRepo.save(entity: entity)
+        }catch{
+            print("Failed to add new topic")
+        }
+    }
+    //    func textViewDidChange(_ textView: UITextView) {
 //        if !textView.text.isEmpty && !isFocusTv {
 //            tvBodyPost.text = "" + textView.text
 //            isFocusTv = true
