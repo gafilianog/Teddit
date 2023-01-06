05ceba0edf8e7b88692ea97f1c8579b613246e77
gafilianog <gafiliano2@gmail.com>
2023-01-05 22:03:15 -0800

    fix banner and sub logo ForumViewController

--- Teddit/ForumViewController.swift
+++ Teddit/ForumViewController.swift
@@ -8,37 +8,47 @@
 import UIKit
 
 class ForumViewController: UIViewController {
-
-    private let floatAddButton: UIButton = {
-        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
-        button.layer.masksToBounds = true
-        button.layer.cornerRadius = 30
-        button.backgroundColor = .blue
-        
-        button.setImage(UIImage(systemName: "plus", withConfiguration: UIImage.SymbolConfiguration(pointSize: 32, weight: .medium)), for: .normal)
-        
-        button.tintColor = .white
-        button.setTitleColor(.white, for: .normal)
-        
-        button.layer.shadowRadius = 10
-        button.layer.shadowOpacity = 0.2
-        
-        return button
-    }()
+    
+    @IBOutlet var imgSubBanner: UIImageView!
+    @IBOutlet var imgSubLogo: UIImageView!
+    
+//    private let floatAddButton: UIButton = {
+//        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+//        button.layer.masksToBounds = true
+//        button.layer.cornerRadius = 30
+//        button.backgroundColor = .blue
+//
+//        button.setImage(UIImage(systemName: "plus", withConfiguration: UIImage.SymbolConfiguration(pointSize: 32, weight: .medium)), for: .normal)
+//
+//        button.tintColor = .white
+//        button.setTitleColor(.white, for: .normal)
+//
+//        button.layer.shadowRadius = 10
+//        button.layer.shadowOpacity = 0.2
+//
+//        return button
+//    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imgSubLogo.setRound()
+//        imgSubBanner.bringSubviewToFront(imgSubLogo)
 
-        view.addSubview(floatAddButton)
-        floatAddButton.addTarget(self, action: #selector(plusPressed), for: .touchUpInside)
+//        view.addSubview(floatAddButton)
+//        floatAddButton.addTarget(self, action: #selector(plusPressed), for: .touchUpInside)
         
         
         // Do any additional setup after loading the view.
     }
     
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        floatAddButton.frame = CGRect(x: view.frame.size.width - 120, y: view.frame.size.width - 120, width: 50, height: 50)
+//        floatAddButton.frame = CGRect(x: view.frame.size.width - 120, y: view.frame.size.width - 120, width: 50, height: 50)
     }
     
     @objc private func plusPressed(){
