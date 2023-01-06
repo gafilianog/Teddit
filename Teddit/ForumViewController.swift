7c00976d015d32737c38e9b29b604a0c6f87dae7
gafilianog <gafiliano2@gmail.com>
2023-01-06 00:47:39 -0800

    add forum page

--- Teddit/ForumViewController.swift
+++ Teddit/ForumViewController.swift
@@ -7,36 +7,41 @@
 
 import UIKit
 
-class ForumViewController: UIViewController {
+class ForumViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet var imgSubBanner: UIImageView!
     @IBOutlet var imgSubLogo: UIImageView!
+    @IBOutlet var tvForumPost: UITableView!
     
-//    private let floatAddButton: UIButton = {
-//        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
-//        button.layer.masksToBounds = true
-//        button.layer.cornerRadius = 30
-//        button.backgroundColor = .blue
-//
-//        button.setImage(UIImage(systemName: "plus", withConfiguration: UIImage.SymbolConfiguration(pointSize: 32, weight: .medium)), for: .normal)
-//
-//        button.tintColor = .white
-//        button.setTitleColor(.white, for: .normal)
-//
-//        button.layer.shadowRadius = 10
-//        button.layer.shadowOpacity = 0.2
-//
-//        return button
-//    }()
+    let titles = ["If elevators hadn't been invented, all the CEOs and important people would have their offices on the first floor as a sign of status.", "If elevators hadn't been invented, all the CEOs.", "If elevators hadn't been invented, all the CEOs and important people would have their offices on the first floor as a sign of status. If elevators hadn't been invented, all the CEOs and important people.", "If elevators hadn't been invented, all the CEOs and important people would have their offices on the first floor as a sign of status.", "If elevators hadn't been invented, all the CEOs.", "If elevators hadn't been invented, all the CEOs and important people would have their offices on the first floor as a sign of status. If elevators hadn't been invented, all the CEOs and important people."]
+    let usernames = ["john_doe", "jane_doe", "pakerte", "john_doe", "jane_doe", "pakerte"]
+    let commentCounts = [100, 0, 9999, 100, 0, 9999]
+    
+    private let floatAddButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = 0.5 * button.bounds.size.width
+        button.backgroundColor = .blue
+
+        button.setImage(UIImage(systemName: "plus", withConfiguration: UIImage.SymbolConfiguration(pointSize: 32, weight: .medium)), for: .normal)
+
+        button.tintColor = .white
+        button.setTitleColor(.white, for: .normal)
+
+        button.layer.shadowRadius = 10
+        button.layer.shadowOpacity = 0.2
+
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         imgSubLogo.setRound()
-//        imgSubBanner.bringSubviewToFront(imgSubLogo)
+        tvForumPost.dataSource = self
 
-//        view.addSubview(floatAddButton)
-//        floatAddButton.addTarget(self, action: #selector(plusPressed), for: .touchUpInside)
+        view.addSubview(floatAddButton)
+        floatAddButton.addTarget(self, action: #selector(plusPressed), for: .touchUpInside)
         
         
         // Do any additional setup after loading the view.
@@ -46,9 +51,21 @@ class ForumViewController: UIViewController {
         return .lightContent
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usernames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tvForumPost.dequeueReusableCell(withIdentifier: "post_cell", for: indexPath) as! PostItemTableViewCell
+        
+        cell.configure(username: usernames[indexPath.row], title: titles[indexPath.row], commentCount: commentCounts[indexPath.row])
+        
+        return cell
+    }
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-//        floatAddButton.frame = CGRect(x: view.frame.size.width - 120, y: view.frame.size.width - 120, width: 50, height: 50)
+        floatAddButton.frame = CGRect(x: view.frame.size.width - 80, y: view.frame.size.height - 90, width: 50, height: 50)
     }
     
     @objc private func plusPressed(){
---
+++ Teddit/PostItemTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  PostItemTableViewCell.swift
+//  Teddit
+//
+//  Created by prk on 05/01/23.
+//
+
+import UIKit
+
+class PostItemTableViewCell: UITableViewCell {
+
+    @IBOutlet var vCardContainer: UIView!
+    @IBOutlet var lblUsername: UILabel!
+    @IBOutlet var lblPostTitle: UILabel!
+    @IBOutlet var lblCommentCount: UILabel!
+    
+    func configure(username: String, title: String, commentCount: Int) {
+        lblUsername.text = "Posted by u/\(username)"
+        lblPostTitle.text = title
+        lblCommentCount.text = String(commentCount)
+        
+        vCardContainer.layer.shadowColor = UIColor.lightGray.cgColor
+        vCardContainer.layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        vCardContainer.layer.shadowOpacity = 0.1
+        vCardContainer.layer.masksToBounds = false
+        vCardContainer.layer.cornerRadius = 10
+    }
+
+}
