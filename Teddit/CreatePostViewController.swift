91ca1f03dbb5e3007819e7b8de37c5d79df3c1f1
gafilianog <gafiliano2@gmail.com>
2023-01-06 22:33:14 -0800

    add create post page

---
+++ Teddit/CreatePostViewController.swift
@@ -0,0 +1,42 @@
+//
+//  CreatePostViewController.swift
+//  Teddit
+//
+//  Created by prk on 06/01/23.
+//
+
+import UIKit
+
+class CreatePostViewController: UIViewController, UITextViewDelegate {
+
+    @IBOutlet var tfPostTitle: UITextField!
+    @IBOutlet var tvPostBody: UITextView!
+    @IBOutlet var lblFakePlaceholder: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tvPostBody.layer.cornerRadius = 6
+        tvPostBody.layer.borderColor = UIColor.gray.withAlphaComponent(0.3).cgColor
+        tvPostBody.layer.borderWidth = 0.5
+        tvPostBody.textColor = .lightGray
+//        tvPostBody.text = "LOREM IPSUM"
+//
+        tvPostBody.delegate = self
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        lblFakePlaceholder.isHidden = true
+//        if tvPostBody.textColor == .lightGray && tvPostBody.isFirstResponder {
+//            tvPostBody.text = nil
+//            tvPostBody.textColor = .black
+//        }
+    }
+    
+//    func textViewDidBeginEditing(_ textView: UITextView) {
+//        if tvPostBody.textColor == .lightGray && tvPostBody.isFirstResponder {
+//            tvPostBody.text = nil
+//            tvPostBody.textColor = .black
+//        }
+//    }
+}
